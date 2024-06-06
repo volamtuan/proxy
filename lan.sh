a4a3f531610cb0100038daddd74b5785b9461807
️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️ <30877561+volamtuan@users.noreply.github.com>
2024-06-06 11:08:14 +0700

    Update lan.sh

--- lan.sh
+++ lan.sh
@@ -18,12 +18,11 @@ install_3proxy() {
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-0.9.4
     make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,stat}
-    cp bin/3proxy /usr/local/etc/3proxy/bin/
-    cp ../init.d/3proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
-    chkconfig 3proxy on
-    cd $WORKDIR
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    #cp src/3proxy /usr/local/etc/3proxy/bin/
+    #chmod +x /etc/init.d/3proxy
+    #chkconfig 3proxy on
+    cd "$WORKDIR" || exit 1
 }
 
 gen_3proxy() {
