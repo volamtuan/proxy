0e12784c53361b6ed50160d49d4575a46d4775b3
️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️ <30877561+volamtuan@users.noreply.github.com>
2024-06-06 11:09:12 +0700

    Update lan.sh

--- lan.sh
+++ lan.sh
@@ -16,7 +16,7 @@ gen64() {
 install_3proxy() {
     URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.4
+    cd /home/3proxy-0.9.4 || exit 1
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     #cp src/3proxy /usr/local/etc/3proxy/bin/
