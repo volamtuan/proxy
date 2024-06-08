4741c2bff01c95ada083d9a400eb665c9b5c06bd
️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️ <30877561+volamtuan@users.noreply.github.com>
2024-06-09 01:34:54 +0700

    Update lan.sh

--- lan.sh
+++ lan.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-start_time=$(date +%s)
-
 random() {
     tr </dev/urandom -dc A-Za-z0-9 | head -c5
     echo
@@ -19,29 +17,33 @@ gen64() {
 install_3proxy() {
     echo "installing 3proxy"
     URL="https://github.com/z3APA3A/3proxy/archive/refs/tags/0.9.3.tar.gz"
-    wget -qO- $URL | tar -xz >/dev/null 2>&1
+    wget -qO- $URL | tar -xz
     cd 3proxy-0.9.3
-    make -f Makefile.Linux >/dev/null 2>&1
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat} >/dev/null 2>&1
-    cp src/3proxy /usr/local/etc/3proxy/bin/ >/dev/null 2>&1
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp src/3proxy /usr/local/etc/3proxy/bin/
     cd $WORKDIR
 }
 
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 4000
+maxconn 10000
 nserver 1.1.1.1
-nserver 8.8.8.8
+nserver 8.8.4.4
 nserver 2001:4860:4860::8888
 nserver 2001:4860:4860::8844
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
-stacksize 6291456
+stacksize 6291456 
+dnsmaxttl 30m
+rotate 30
 flush
 auth none
+allow 14.224.163.75
+allow 127.0.0.1
 
 $(awk -F "/" '{print "allow *\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
@@ -51,10 +53,9 @@ EOF
 
 gen_proxy_file_for_user() {
     cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+$(awk -F "/" '{print $3 ":" $4}' ${WORKDATA})
 EOF
 }
-
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
         echo "//$IP4/$port/$(gen64 $IP6)"
@@ -63,7 +64,7 @@ gen_data() {
 
 gen_iptables() {
     cat <<EOF
-$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT >/dev/null 2>&1"}' ${WORKDATA})
+$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 " -m state --state NEW -j ACCEPT"}' ${WORKDATA})
 EOF
 }
 
@@ -73,21 +74,12 @@ $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 
-download_proxy() {
-    cd $WORKDIR || return
-    curl -F "file=@proxy.txt" https://file.io >/dev/null 2>&1
-}
-
 echo "installing apps"
-echo "Cài đặt các gói cần thiết..."
-yum -y install gcc net-tools bsdtar zip make >/dev/null 2>&1
-yum install curl wget -y >/dev/null 2>&1
-yum install nano net-tools -y >/dev/null 2>&1
-echo "Hoàn tất cài đặt các gói cần thiết."
+yum -y install gcc net-tools tar zip >/dev/null
 
 install_3proxy
 
-echo "Đang thiết lập Thư Mục..."
+echo "working folder = /home/proxy"
 WORKDIR="/home/proxy"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
@@ -97,11 +89,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
-FIRST_PORT=27000
-LAST_PORT=30000
-
-echo "Cổng proxy: $FIRST_PORT"
-echo "Số lượng proxy tạo: $(($LAST_PORT - $FIRST_PORT + 1))"
+FIRST_PORT=20000
+LAST_PORT=22222
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
@@ -116,15 +105,9 @@ bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 20048
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 EOF
-chmod +x /etc/rc.local
+
 bash /etc/rc.local
-rm -rf lan.sh
-rm -rf /root/3proxy-0.9.3
 
 gen_proxy_file_for_user
 
 echo "Starting Proxy"
-
-echo “Tổng số IPv6 hiện tại:”
-ip -6 addr | grep inet6 | wc -l
-download_proxy
